01ebf86c2f9a1db37339996bbaa799100dd8b6b6
KuWojto <kwojto07@gmail.com>
2023-11-19 18:33:49 +0100

    Utests init

---
+++ cpp/GildedRose.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+class Item
+{
+public:
+    ::std::string name;
+    int sellIn;
+    int quality;
+    Item(::std::string name, int sellIn, int quality) : name(::std::move(name)), sellIn(sellIn), quality(quality)
+    {}
+};
+
+class GildedRose
+{
+
+public:
+    ::std::vector<Item> items;
+    explicit GildedRose(::std::vector<Item> && items);
+    explicit GildedRose(::std::vector<Item> const& items);
+
+    void updateQuality();
+};
+
