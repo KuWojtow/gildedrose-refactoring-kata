7a3ed772ed94ae2f692ca41b0876bb9450d92aee
KuWojto <kwojto07@gmail.com>
2023-11-20 18:39:19 +0100

    rozwiazanie funkcjami

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,84 +1,69 @@
 #include "GildedRose.h"
 
-
 GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 {}
-
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
 
+void IncreaseQuality(Item& item){
+    if (item.quality<50){
+        item.quality ++;
+    }
+}
+void DecreaseQuality(Item& item){
+    if (item.quality>0){
+        item.quality--;
+    }
+}
+void UpdateB(Item& item){
+    IncreaseQuality(item);
+    if (item.sellIn<11){
+        IncreaseQuality(item);
+    }
+    if (item.sellIn<6){
+        IncreaseQuality(item);
+    }
+}
+
 void GildedRose::updateQuality()
 {
-    for (int i = 0; i < items.size(); i++)
-    {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        for (int i = 0; i < items.size(); i++)
         {
-            if (items[i].quality > 0)
+            if (items[i].name == "Aged Brie")
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
+                IncreaseQuality(items[i]);
             }
-        }
-        else
-        {
-            if (items[i].quality < 50)
+            else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
+                UpdateB(items[i]);
             }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
+            else if (items[i].name != "Sulfuras, Hand of Ragnaros")
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                DecreaseQuality(items[i]);
+            }
+
+            if (items[i].name != "Sulfuras, Hand of Ragnaros")
+            {
+                items[i].sellIn--;
+            }
+
+            if (items[i].sellIn < 0)
+            {
+                if (items[i].name == "Aged Brie")
                 {
-                    if (items[i].quality > 0)
+                    IncreaseQuality(items[i]);
+                }
+                else if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                        DecreaseQuality(items[i]);
                     }
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality = 0;
                 }
             }
         }
     }
-}
